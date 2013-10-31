fbfa43ec6e43eb7e4c2fccf9476a744362cd5266
Tony Aldridge <zaragopha@hotmail.com>
2013-10-31 21:05:33 +0000

    Basic parts for restarting are all in

--- retrigger.tcl
+++ retrigger.tcl
@@ -15,14 +15,56 @@ proc get { url } {
     return [process_token [http::geturl $url]]
 }
 
+proc authenticated_get { url token } {
+    return [process_token [http::geturl $url -headers [get_auth_headers $token] ] ]
+}
+
 proc post { url data } {
     return [process_token [http::geturl $url -query [generate_query_params $data] ] ]
 }
 
+proc authenticated_post { url data token } {
+    return [process_token [http::geturl $url -query [generate_query_params $data] -headers [get_auth_headers $token] ] ]
+}
+
 proc generate_query_params { items } {
-    return http::formatQuery {*}$items
+    return [http::formatQuery {*}$items]
+}
+
+proc get_github_token { } {
+    set fp [open "token.txt" r]
+    set filedata [read $fp]
+    close $fp
+    return $filedata
+}
+
+proc get_repo_list {} {
+    set fp [open "token.txt" r]
+    set filedata [read $fp]
+    close $fp
+    return $filedata
+}
+
+proc get_auth_headers { token } {
+    return [list Authorization "token $token"]
+}
+
+proc get_travis_token {} {
+    set result [post https://api.travis-ci.org/auth/github "github_token [get_github_token]"]
+    return [dict get $result access_token]
+}
+
+proc get_last_master_build { repo } {
+    set result [get https://api.travis-ci.org/repos/$repo/branches/master]
+    return [dict get $result branch id]
+}
+
+proc restart_build { build_id token } {
+    set result [authenticated_post https://api.travis-ci.org/requests "build_id $build_id" $token]
+    return $result
 }
 
 http::register https 443 ::tls::socket
-set params {}
-puts [post https://api.travis-ci.org/config $params]
+set token [get_travis_token]
+set build_id [get_last_master_build AngryLawyer/rust-apl]
+puts [restart_build $build_id $token]
