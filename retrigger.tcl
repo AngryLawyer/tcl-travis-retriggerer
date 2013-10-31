a0eb1bad24e6a75fdad3ea52be2da11e167fb117
Tony Aldridge <tony@dabapps.com>
2013-10-31 14:00:39 +0000

    Added ability to POST to the API

--- retrigger.tcl
+++ retrigger.tcl
@@ -1,13 +1,37 @@
 #!/usr/bin/tclsh
+package require Tcl 8.5
 package require http
 package require tls
 package require json
 
-proc get { url } {
-    set token [http::geturl $url]
+proc process_token { token } {
     upvar #0 $token state
     return [json::json2dict $state(body)]
 }
 
+proc get { url } {
+    return [process_token [http::geturl $url]]
+}
+
+proc post { url data } {
+    return [process_token [http::geturl $url -query [generate_query_params $data] ]]
+}
+
+proc generate_query_params { items } {
+    set result ""
+    set first true
+    dict for {key value} $items {
+        if {$first == true} {
+            set first false
+        } else {
+            append result "&"
+        }
+
+        append result $key=$value
+    }
+    return $result
+}
+
 http::register https 443 ::tls::socket
-puts [get https://api.travis-ci.org/config]
+set params {}
+puts [post https://api.travis-ci.org/config $params]
